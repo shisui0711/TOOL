c6f8442e89a7e4831db040e59c0c8117007ad1d3
Khang <khangnguyenhy2004@gmail.com>
2022-11-25 14:18:54 +0700

    Update knv.py

--- knv.py
+++ knv.py
@@ -37,7 +37,7 @@ def banner():
     ║ ➽ Facebook : Nguyễn Văn Khang   ║ 
     ║ ➽ Zalo : 0988655794             ║
     ║ ➽ Youtube : Shisui0711          ║
-    ║ ➽ Bản Quyền : Shisui0711        
+    ║ ➽ Bản Quyền : Shisui0711        ║
     ╚═════════════════════════════════╝                                                                                             
                                \n\n'''
         print(Colorate.Vertical(Colors.DynamicMIX((Col.light_green, Col.light_blue)), Center.XCenter(banner)))
